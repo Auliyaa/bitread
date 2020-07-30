6dbd6711855c4f023f863675832457f1963ce9fc
Ludwig Fiolka <urushiraa@gmail.com>
2020-07-30 11:18:07 +0200

    Update interruptible_sleep.h

--- chrono/interruptible_sleep.h
+++ chrono/interruptible_sleep.h
@@ -1,8 +1,10 @@
+/// a interruptible sleep function based on a bool-castable switch
 template<typename R1, typename P1,
+         typename SW,
          typename R2=int64_t, typename P2=std::ratio<1,1000>>
-bool _sleep_for(const std::chrono::duration<R1,P1>& duration,
-                bool& sw,
-                const std::chrono::duration<R2,P2>& check_frequency = std::chrono::milliseconds(500))
+bool sleep_for_inter(const std::chrono::duration<R1,P1>& duration,
+                     SW& sw,
+                     const std::chrono::duration<R2,P2>& check_frequency = std::chrono::milliseconds(500))
 {
   auto start = std::chrono::steady_clock::now();
   while(sw)
@@ -12,7 +14,7 @@ bool _sleep_for(const std::chrono::duration<R1,P1>& duration,
     {
       return sw;
     }
-    std::this_thread::sleep_for(check_frequency);
+    std::this_thread::sleep_for(std::min(check_frequency, duration - now - start));
   }
   return sw;
 }
