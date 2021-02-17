58af1e8239f6869a789ec52b0e50f11ff35867cb
Ludwig Fiolka <urushiraa@gmail.com>
2021-02-17 16:17:58 +0100

    Create map_range.h

---
+++ map_range/map_range.h
@@ -0,0 +1,6 @@
+#pragma once
+
+auto map_range = [](double x, double in_min, double in_max, double out_min, double out_max) -> double
+        {
+          return (x-in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        };
